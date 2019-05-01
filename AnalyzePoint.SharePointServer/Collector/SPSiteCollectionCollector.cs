a11249cfb929e92a082c73fbe34a9e6e66ed1038
zsigmonda <11335646+zsigmonda@users.noreply.github.com>
2019-05-02 00:21:16 +0200

    New collectors were added.

---
+++ AnalyzePoint.SharePointServer/Collector/SPSiteCollectionCollector.cs
@@ -0,0 +1,28 @@
+﻿using AnalyzePoint.Core.Collector;
+using AnalyzePoint.Core.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnalyzePoint.SharePointServer.Collector
+{
+  public class SPSiteCollectionCollector : ComponentCollector
+  {
+    public override ComponentCollector ForComponent(object componentToProcess)
+    {
+      throw new NotImplementedException();
+    }
+
+    public override Descriptor Process()
+    {
+      throw new NotImplementedException();
+    }
+
+    public override Descriptor Process(object componentToProcess)
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
